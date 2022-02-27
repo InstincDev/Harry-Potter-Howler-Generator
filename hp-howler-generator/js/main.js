ebf7c822a13edfe077a620b9b62531916400eb19
ChanelT <67307808+InstincDev@users.noreply.github.com>
2022-02-27 08:16:55 -0600

    initial commit

---
+++ hp-howler-generator/js/main.js
@@ -0,0 +1,21 @@
+
+// eventListener and function call
+document.getElementById("yell").addEventListener('click', yellAt)
+// insert name into DOM
+
+function yellAt(event){
+    event.preventDefault();
+
+    document.querySelector('body').style.backgroundColor = '#222f5b';
+
+    document.querySelector('.placeForName').classList.toggle('hidden');
+
+    document.querySelector('.title').style.color = "#727272";
+
+    // get name values and put in vars
+    const fname = document.querySelector(".fname").value;
+    const mname = document.querySelector(".mname").value;
+    const lname = document.querySelector(".lname").value;
+
+    document.querySelector(".name").innerText = `${fname} ${mname} ${lname}`;
+}

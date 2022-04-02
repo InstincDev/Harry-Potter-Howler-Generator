17ee9858e63a3caecfa85d3a47d441a89b10d38f
ChanelT <crene7722@gmail.com>
2022-04-02 10:32:07 -0500

    add voice synth

--- hp-howler-generator/js/main.js
+++ hp-howler-generator/js/main.js
@@ -1,22 +1,41 @@
 
-// eventListener and function call
-document.getElementById("yell").addEventListener('click', yellAt)
-// insert name into DOM
+// // eventListener and function call
+// document.getElementById("yell").addEventListener('click', yellAt)
+// // insert name into DOM
 
-function yellAt(event){
-    event.preventDefault();
+// function yellAt(event){
+//     event.preventDefault();
 
-    document.querySelector('body').style.backgroundColor = '#222f5b';
+//     document.querySelector('body').style.backgroundColor = '#222f5b';
+
+//     document.querySelector('.placeForName').classList.toggle('hidden');
+
+//     document.querySelector('.title').style.color = "#727272";
+
+//     // get name values and put in vars
+//     const fname = document.querySelector(".fname").value;
+//     const mname = document.querySelector(".mname").value;
+//     const lname = document.querySelector(".lname").value;
+
+//     document.querySelector(".name").innerText = `${fname} ${mname} ${lname}`;
+//    document.querySelector('form').reset(); 
+// }
+
+const synth = window.speechSynthesis;
+document.getElementById("yell").addEventListener('click', mollyWeasley)
+
+function mollyWeasley(){
+    let fname = document.querySelector('.fname').value;
+    let mname = document.querySelector('.mname').value;
+    let lname = document.querySelector('.lname').value;
 
     document.querySelector('.placeForName').classList.toggle('hidden');
-
+    document.querySelector('body').style.backgroundColor = '#222f5b'
     document.querySelector('.title').style.color = "#727272";
+    document.querySelector('.name').innerText = `${fname} ${mname} ${lname}`;
+    let yell = document.querySelector('.placeForName').innerText;
+    document.querySelector('form').reset();
+    let yellThis = new SpeechSynthesisUtterance(yell);
 
-    // get name values and put in vars
-    const fname = document.querySelector(".fname").value;
-    const mname = document.querySelector(".mname").value;
-    const lname = document.querySelector(".lname").value;
-
-    document.querySelector(".name").innerText = `${fname} ${mname} ${lname}`;
-   document.querySelector('form').reset(); 
+  synth.speak(yellThis);
 }
